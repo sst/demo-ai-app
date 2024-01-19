ff5c06a7bcc1ebfd15feaef7afabbe5f755c1047
Jay <air@live.ca>
2024-01-19 13:55:43 -0500

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,9 +1,4 @@
-import { Resource } from "sst";
-
 export default {
   region: "us-east-1",
-  //db: Resource.Movies.tableName,
-  //bucket: Resource.Assets.bucketName,
-  //vectorClient: Resource.Vector.retrieverFunctionName,
   repo: "https://github.com/sst/demo-ai-app",
 };
